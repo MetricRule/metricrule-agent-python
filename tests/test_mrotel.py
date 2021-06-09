b7f429c37a863306c5842066d8d7b2684206bf1d
Jishnu Mohan <mohanjishnu@gmail.com>
2021-06-09 16:35:48 +0800

    Add tests for MROtel

--- tests/test_mrotel.py
+++ tests/test_mrotel.py
@@ -1,10 +1,74 @@
 from unittest import TestCase
+from unittest.mock import MagicMock, Mock
+from unittest import main
 
-from metricrule.agent import mrotel
+from opentelemetry import metrics
+
+from metricrule.agent.mrotel import *
+from metricrule.agent.mrmetric import MetricInstrumentSpec
 
 class TestMrOtel(TestCase):
     def test_initialize_counter(self):
-        pass
+        meter = MagicMock(metrics.Meter)
+        created_counter = Mock(Counter)
+        name = 'test_counter_init'
+        spec = MetricInstrumentSpec(
+            metrics.Counter,
+            int,
+            name,
+        )
+        meter.create_counter.return_value = created_counter
+
+        counter = initialize_instrument(meter, spec)
+
+        meter.create_counter.assert_called_with(
+            name=name, 
+            description='', 
+            unit='', 
+            value_type=int)
+        self.assertIsInstance(counter, Counter)
+        self.assertEqual(counter.counter, created_counter)
+
 
     def test_initialize_float_recorder(self):
-        pass
+        meter = MagicMock(metrics.Meter)
+        created_recorder = Mock(Counter)
+        name = 'test_recorder_init'
+        spec = MetricInstrumentSpec(
+            metrics.ValueRecorder,
+            float,
+            name,
+        )
+        meter.create_valuerecorder.return_value = created_recorder
+
+        recorder = initialize_instrument(meter, spec)
+
+        meter.create_valuerecorder.assert_called_with(
+            name=name, 
+            description='', 
+            unit='', 
+            value_type=float)
+        self.assertIsInstance(recorder, ValueRecorder)
+        self.assertEqual(recorder.recorder, created_recorder)
+    
+    def test_counter_record(self):
+        meter = MagicMock(metrics.Meter)
+        created_counter = Mock(Counter)
+        name = 'test_counter_init'
+        spec = MetricInstrumentSpec(
+            metrics.Counter,
+            int,
+            name,
+        )
+        meter.create_counter.return_value = created_counter
+        counter = initialize_instrument(meter, spec)
+
+        recording = counter.record(1)
+
+        self.assertIsInstance(recording, tuple)
+        self.assertEqual(len(recording), 2)
+        self.assertEqual(recording[0], created_counter)
+        self.assertEqual(recording[1], 1)
+
+if __name__ == '__main__':
+    main()
